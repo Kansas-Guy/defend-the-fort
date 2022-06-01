08bd721c8755042fb4c9aa037f8edcf8b935ed20
Zach <87861134+Kansas-Guy@users.noreply.github.com>
2022-06-01 09:27:01 -0500

    Add list with variable url path to contact

--- contact/urls.py
+++ contact/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from . import views
 
+app_name = 'contact'
 urlpatterns = [
     # ex: /contact/
     path('', views.index, name='index'),
     # ex: /contact/student
-    path('student/', views.student, name='student'),
+    # path('student/<str:team>/', views.student, name='student'),
+    path('<str:team>', views.student, name='student'),
     # ex: /contact/donor
     path('donor/', views.donor, name='donor')
-         ]
+]
