5af791abbc6a3b000619bd4d7f6994c8d8446d03
Zach <87861134+Kansas-Guy@users.noreply.github.com>
2022-06-05 08:01:46 -0500

    Update admin.py with Roster model

--- contact/admin.py
+++ contact/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Team, Student, Donor
+from .models import Team, Roster, StudentInfo, Donor
 
 # Register your models here.
 
-admin.site.register([Team, Student, Donor])
+admin.site.register([Team, Roster, StudentInfo, Donor])
