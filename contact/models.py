6112634c00b0da89394c8daf3bee00c292ec46b1
Zach <87861134+Kansas-Guy@users.noreply.github.com>
2024-10-16 15:24:31 -0500

    Add project_link to project models.py

--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class Project(models.Model):
     project_name = models.CharField(max_length=50)
+    project_link = models.URLField(max_length=200, blank=True)
 
     def __str__(self):
         return self.project_name
